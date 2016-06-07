bfdd6837ee58bd6ea9d23a8242526c09894660d2
Yuri do Carmo <yurimdocarmo@gmail.com>
2016-06-06 21:36:52 -0300

    Add graph

--- src/scheduler/DeadlockManager.java
+++ src/scheduler/DeadlockManager.java
@@ -1,17 +1,13 @@
 package scheduler;
 
-import java.util.List;
-import java.util.Scanner;
-
-import xml.Aresta;
-import xml.GeradorXML;
-import xml.Vertice;
+import graph.*;
 
 public class DeadlockManager {
 
 	public enum PreventionType {WAIT_DIE, WOUND_WAIT};
-	public static GeradorXML gerador = new GeradorXML();
 	
+	private Digraph waitingGraph;
+	private DirectedCycle directedGraph;
 	
 	/**
 	 * TODO: 
@@ -24,72 +20,19 @@ public class DeadlockManager {
 	
 	public DeadlockManager(final PreventionType preventionType) {
 		this.preventionType = preventionType;
+		this.waitingGraph = new Digraph(10);
+		this.directedGraph = new DirectedCycle(this.waitingGraph);
 	}
 	
-	public void addEdgeIntoGraph(Aresta aresta) {
-		gerador.addAresta(aresta);
+	public void addEdgeIntoGraph(int source, int target) {
+		waitingGraph.addEdge(source, target);
 	}
 	
-	public void removeEdgeFromGraph(Aresta aresta) {
-		gerador.removerAresta(aresta);
-	}
-	
-	public void addNodeIntoGraph(Vertice vertice){
-		gerador.addVertice(vertice);	
-	}
-	
-	public void removeNodeFromGraph(Vertice vertice){
-		gerador.removerVertice(vertice);
+	public void removeEdgeFromGraph() {
+		//TODO
 	}
 	
 	public boolean graphIsCyclic() {
-		return false;
-	}
-	
-	public static void main(String[] args) {
-		DeadlockManager deadlockManager = new DeadlockManager(DeadlockManager.PreventionType.WAIT_DIE);
-		Integer id = 0;
-		boolean continua = true;
-		while (continua) {
-			id++;
-			Scanner read = new Scanner(System.in);
-			Scanner r = new Scanner(System.in);
-
-			System.out.println("OPERACAO 1 - VERTICE | 2 - ARESTA");
-			Integer operacao = r.nextInt();
-			if (operacao.equals(1)) {
-
-				System.out.println("Digite o nome");
-				String nome = read.nextLine();
-				Vertice vertice = new Vertice(id.toString(), nome, id.toString());
-				deadlockManager.addNodeIntoGraph(vertice);
-
-			}else if(operacao.equals(2)){
-				System.out.println("ARESTA - ADD - 1 REMOVE - 2");
-				Integer respos = r.nextInt();
-
-				System.out.println("VERTICE A");
-				String a = read.nextLine();
-
-				System.out.println("VERTICE B");
-				String b = read.nextLine();
-				
-				Vertice verticeA = new Vertice(a, "", a);
-				Vertice verticeB = new Vertice(b, "", a);
-				Aresta aresta = new Aresta("1", verticeA, verticeB, "10");
-
-				
-				
-						if(respos.equals(1)){
-							deadlockManager.addEdgeIntoGraph(aresta);
-
-						}else if(respos.equals(2)){
-							deadlockManager.removeEdgeFromGraph(aresta);
-						}
-						
-				
-				
-			}
-		}
+		return directedGraph.hasCycle();
 	}
 }
